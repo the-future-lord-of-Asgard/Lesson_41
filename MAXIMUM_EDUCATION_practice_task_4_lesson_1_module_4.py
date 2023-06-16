9c6692f76210f54b369a51204b357624eb36db38
the-future-lord-of-Asgard <136183650+the-future-lord-of-Asgard@users.noreply.github.com>
2023-06-16 21:13:08 +0300

    added practice1
    Описание реализации алгоритма проверки строки на палиндром:
    Для проверки слова я создала функцию palindrome, которая выполняется в переменной result и проверяет равна ли переменная first_string (туда пользователь вводит слово) той же переменной записанной наоборот, выдавая в конечном результате логическое значение сравнения.

---
+++ MAXIMUM_EDUCATION_practice_task_4_lesson_1_module_4.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+def palindrome():
+    return first_string == first_string[::-1]
+
+first_string = input()
+first_string = first_string
+
+result = palindrome()
+print(result)
+

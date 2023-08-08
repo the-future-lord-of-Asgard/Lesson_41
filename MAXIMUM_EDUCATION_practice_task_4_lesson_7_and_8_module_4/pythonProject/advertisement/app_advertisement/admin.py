4fba550739f6940cf7d95d5beca7845f1278faf2
the-future-lord-of-Asgard <136183650+the-future-lord-of-Asgard@users.noreply.github.com>
2023-08-08 21:41:25 +0300

    added practice for lesson 7-8

---
+++ MAXIMUM_EDUCATION_practice_task_4_lesson_7_and_8_module_4/pythonProject/advertisement/app_advertisement/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from .models import Advertisements81
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'auction', 'bu', 'created_date', 'updated_date', 'image']
+    list_filter = ['auction', 'bu', 'created_time']
+    actions = ['make_auction_as_false', 'make_auction_as_true']
+    fieldsets = (
+        ('Общее', {
+            'fields': ('title', 'description', 'bu', 'images')
+        }),
+        ('Финансы', {
+            'fields': ('price', 'auction'),
+            'classes': ['collapse']
+        })
+    )
+
+    @admin.action(description='Убрать возможность торга.')
+    def make_auction_as_false(self, request, queryset):
+        queryset.update(auction=False)
+
+    @admin.action(description= 'Добавить возможность торга.')
+    def make_auction_as_true(self, request, queryset):
+        queryset.update(auction=True)
+
+admin.site.register(Advertisements81, AdvertisementAdmin)
